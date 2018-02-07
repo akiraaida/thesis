d8f6fed12fa2bc7c733ac405b302338786ad0315
Akira <akira.aida@uoit.net>
2018-02-07 17:16:35 -0500

    Consolidated the two programs into one
    - Consolidated the prep and rwr

--- src/prep/build.sbt
+++
@@ -1,7 +0,0 @@
-name := "PrepData"
-
-version := "1.0"
-
-scalaVersion := "2.11.8"
-
-libraryDependencies += "org.apache.spark" %% "spark-mllib" % "2.2.0"
--- src/src/main/scala/RWR.scala
+++ src/src/main/scala/RWR.scala
@@ -70,6 +70,33 @@ object PrepData {
       }
     })
 
+    // Read in the sparse transition matrix (the text file is opened distributedly)
+    // Split each line of the file by the "," and then match the resulting values to
+    // a MatrixEntry object. Doing the proper conversion along the way
+    val matrixEntries = csv.map(_.split(",") match {
+      case Array(key, row, col, value) => MatrixEntry(row.toLong, col.toLong, value.toDouble)
+    })
+
+    // Construct the sparse coordinate matrix object
+    val matrix = new CoordinateMatrix(matrixEntries.rdd).toIndexedRowMatrix()
+
+    /*
+    // Figure out the similarity
+    val n = 10
+    for(i <- 1 to n) {
+      val result = matrix.multiply(column)
+      column = result.toBlockMatrix.toLocalMatrix
+
+      if (i == n) {
+        val output = result.rows.map(_.vector.toDense)
+        output.coalesce(1, true).saveAsTextFile("result")
+      }
+    }
+    */
+
+
+
+    /*
     // Create csv values for each matrix entry
     val saveData = transition.map(_ match {
       case (key, row, col, value) => {
@@ -79,6 +106,7 @@ object PrepData {
 
     // Save the matrix to sparse
     saveData.saveAsTextFile("transition")
+    */
 
     // Clean up
     sc.stop()
