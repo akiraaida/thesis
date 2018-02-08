a9ce52a05e0c533ea00c752e70d517a8a124413e
Akira <akira.aida@uoit.net>
2018-02-08 14:59:52 -0500

    Changed Matrix Multiplication
    - Changed matrix multiplication to do it manually instead of using
      a library to do it.
    - Added the (1- B) * e_N portion of the algorithm

--- src/src/main/scala/RWR.scala
+++ src/src/main/scala/RWR.scala
@@ -1,7 +1,5 @@
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.rdd.RDD
-import org.apache.spark.mllib.linalg.distributed.{CoordinateMatrix, MatrixEntry}
-import org.apache.spark.mllib.linalg.{Matrix, Matrices}
 
 object RWR {
   def main(args: Array[String]) {
@@ -28,7 +26,7 @@ object RWR {
         actionLatency.toDouble)
     }).rdd.zipWithIndex
 
-    // Assign edges to all of the nodes in the data set
+    // Assign edges to all of the nodes in the data set. Assigning the index at this point
     val mapData = filterData.flatMap(_ match {
       case (data, index) => {
         data match {
@@ -55,10 +53,8 @@ object RWR {
       }
     })
 
-    // Collect the tuple map onto the driver node, convert it into a key -> val hashmap. Then
-    // broadcast the reference map to each node. The collect operation is needed to convert the data
-    // structure into a map from an RDD tuple. The size of the map should be relatively small
-    // so this should not be an issue memory wise for the driver node.
+    // Collect the tuple map as a hashmap onto the driver node and broadcast it to all nodes.
+    // This will be used to lookup the row/columns
     val refMap = sc.broadcast(refMapTuple.collectAsMap)
 
     // Get only the player nodes
@@ -66,46 +62,55 @@ object RWR {
       case (key, index) => key.contains("player")
     })
 
-    // Take the first player as the player that will be looked for
+    // Take the first player. This will be the player that we will find the similarity for
     val targetPlayer = sc.broadcast(players.collect().head)
 
-    // Create the column object
-    val col = refMapTuple.flatMap(tuple => {
-      if (tuple != targetPlayer.value) {
-        Array(0.0)
-      } else {
-        Array(1.0)
-      }
-    }).collect()
+    // Create the column matrix. Initially it is a matrix with all 0s except for the location
+    // where the target player would be located
+    val colMatrix = sc.broadcast(Array(((targetPlayer.value._2, 0), 1)))
 
-    var columnMatrix = Matrices.dense(col.size, 1, col)
+    // Determine the (1 - B) * e_N portion. The assumption is that e_N is a column vector
+    // with 1 in a specific spot for target player
+    val initial = refMapTuple.map(_ match {
+      case (data, index) => {
+        ((targetPlayer.value._2, index), 1 - BETA)
+      }
+    })
 
     // Create the transition matrix, using the key's unique id as the column and the edge's unique
     // id as the row. Calculate the value by dividing 1 (100%) by the number of edges for the key.
+    // Then multiply it by beta (the chance the walker will walk at random. Afterwards, union
+    // the initial matrix and reduce the values by the key getting the simplified transition
+    // matrix.
     val transition = mapData.flatMap(_ match {
       case (data, index) => {
         data match {
           case (key, edges) => {
             edges.map(edge => {
-              (key, refMap.value(edge), refMap.value(key), (1.0 / edges.size * BETA))
+              ((refMap.value(edge), refMap.value(key)), (1.0 / edges.size) * BETA)
             })
           }
         }
       }
-    })
+    }).union(initial).reduceByKey(_ + _)
 
-    val matrixEntries = transition.map(_ match {
-      case (key, row, col, value) => MatrixEntry(row.toLong, col.toLong, value.toDouble)
-    })
-
-    val matrix = new CoordinateMatrix(matrixEntries).toIndexedRowMatrix()
-
-    // Figure out the similarity
-    val n = 10
-    for(i <- 1 to n) {
-      val result = matrix.multiply(columnMatrix)
-      columnMatrix = result.toBlockMatrix.toLocalMatrix
-    }
+    // Execute the matrix multiplication, only keeping the values that are > 0. Then reduce the
+    // values by their key
+    val result = transition.flatMap(_ match {
+      case ((row, col), value) => {
+        colMatrix.value.map(_ match {
+          case ((row2, col2), value2) => {
+            if (col == row2) {
+             ((row, 0), (value * value2).toDouble)
+            } else {
+             ((row, 0), 0.0)
+            }
+          }
+        })
+      }
+    }).filter(_ match {
+      case ((row, col), value) => value > 0
+    }).reduceByKey(_ + _)
 
     // Clean up
     sc.stop()
